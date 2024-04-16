edb4c4fe16cb957ca88c3ea3bc3ea6df9c32dd79
kolya6ka <127345144+kolya6ka@users.noreply.github.com>
2024-04-16 19:26:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to kaktusrold","Смотрит KaktusRold"];
 
 
 let currentIndex = 0;
