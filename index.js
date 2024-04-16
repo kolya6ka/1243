c180d84031f72836258b137073cc4e839e202c03
kolya6ka <127345144+kolya6ka@users.noreply.github.com>
2024-04-16 19:40:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,10 @@ const client = new Client({
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('YaY Your Bot Status Changed✨');
+  res.send('Бот работает в штатном режиме');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Powered By kolya6ka`);
 });
 
 
@@ -91,9 +90,7 @@ function updateStatusAndSendMessages() {
 }
 
 client.once('ready', () => {
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Бот готов к работе ${client.user.tag}`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
